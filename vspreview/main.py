0dd8b29f5da04da4b45b0a71b3bb7b9ddda7fdac
ShirahaneSuoh03 <91019145+ShirahaneSuoh03@users.noreply.github.com>
2023-12-03 13:13:43 +0800

    add shortcuts for zoom level

--- vspreview/main.py
+++ vspreview/main.py
@@ -141,6 +141,11 @@ class MainToolbar(AbstractToolbar):
         add_shortcut(Qt.Qt.Key_S,         self.sync_outputs_checkbox.click)
         add_shortcut(Qt.Qt.CTRL               + Qt.Qt.Key_Tab, lambda: self.main.switch_output(self.outputs_combobox.currentIndex() + 1))
         add_shortcut(Qt.Qt.CTRL + Qt.Qt.SHIFT + Qt.Qt.Key_Tab, lambda: self.main.switch_output(self.outputs_combobox.currentIndex() - 1))
+        # Experimental new shortcuts
+        add_shortcut(Qt.Qt.Key_Equal, lambda : self.zoom_combobox.setCurrentIndex((self.zoom_combobox.currentIndex() + 1)
+                                                                                  if self.zoom_combobox.currentIndex() < len(self.zoom_levels) - 1 else 1))
+        add_shortcut(Qt.Qt.Key_Minus, lambda : self.zoom_combobox.setCurrentIndex((self.zoom_combobox.currentIndex() - 1)
+                                                                                  if self.zoom_combobox.currentIndex() > 1 else 1))
 
         set_qobject_names(self)
 
